a0eeb3eed371011e8ca45b17507ad740ac02eae5
Marco Antonio Brandini Mendes <137082335+marcobmendes@users.noreply.github.com>
2023-08-23 07:51:16 -0300

    Método "for in( )" para mostrar os vetores.

---
+++ exercicios/ex020/vetornatela2.js
@@ -0,0 +1,9 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+console.log(valores)
+valores.sort()
+console.log(valores)
+
+for (let pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
