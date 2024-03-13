e33909c1caf01c6cce7fdb320376613f62c01815
Marco Antonio Brandini Mendes <137082335+marcobmendes@users.noreply.github.com>
2024-03-13 09:42:35 -0300

    Refazendo o Exercício (Parte 2).
    Refazendo o ex014.

---
+++ revisando/revisando por aula/aula12exRemake/ex014/script.js
@@ -0,0 +1,16 @@
+var agora = new Date()
+var hora = agora.getHours()
+var txt = document.querySelector('div#txt')
+var img = document.querySelector('div#img')
+txt.innerHTML = `Agora são ${hora} horas.`
+
+if (hora >= 6 && hora < 12) {
+    document.body.style.backgroundColor = '#E2CD9F'
+    img.innerHTML = '<img src="images/morning.png" alt="Foto Dia"></img>'
+} else if (hora >= 12 && hora < 18) {
+    document.body.style.backgroundColor = '#b9846f'
+    img.innerHTML = '<img src="images/afternoon.png" alt="Foto Tarde"></img>'
+} else {
+    document.body.style.backgroundColor = '#515154'
+    img.innerHTML = '<img src="images/morning.png" alt="Foto Dia"></img>'
+}
