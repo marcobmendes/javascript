6327bc4ca9ae6d0bfa54c2ad2809f415809a3a91
Marco Antonio Brandini Mendes <137082335+marcobmendes@users.noreply.github.com>
2024-03-13 14:23:51 -0300

    Atualizando ex014 remake.
    Corrigindo alguns detalhes do código.

--- revisando/revisando por aula/aula12exRemake/ex014/script.js
+++ revisando/revisando por aula/aula12exRemake/ex014/script.js
@@ -1,16 +1,18 @@
-var agora = new Date()
-var hora = agora.getHours()
-var txt = document.querySelector('div#txt')
-var img = document.querySelector('div#img')
-txt.innerHTML = `Agora são ${hora} horas.`
+function carregar() {
+    var agora = new Date()
+    var hora = agora.getHours()
+    var txt = document.querySelector('div#txt')
+    var img = document.querySelector('div#img')
+    txt.innerHTML = `Agora são ${hora} horas.`
 
-if (hora >= 6 && hora < 12) {
-    document.body.style.backgroundColor = '#E2CD9F'
-    img.innerHTML = '<img src="images/morning.png" alt="Foto Dia"></img>'
-} else if (hora >= 12 && hora < 18) {
-    document.body.style.backgroundColor = '#b9846f'
-    img.innerHTML = '<img src="images/afternoon.png" alt="Foto Tarde"></img>'
-} else {
-    document.body.style.backgroundColor = '#515154'
-    img.innerHTML = '<img src="images/morning.png" alt="Foto Dia"></img>'
+    if (hora >= 6 && hora < 12) {
+        document.body.style.backgroundColor = '#E2CD9F'
+        img.innerHTML = '<img src="images/morning.png" alt="Foto Dia"></img>'
+    } else if (hora >= 12 && hora < 18) {
+        document.body.style.backgroundColor = '#b9846f'
+        img.innerHTML = '<img src="images/afternoon.png" alt="Foto Tarde"></img>'
+    } else {
+        document.body.style.backgroundColor = '#515154'
+        img.innerHTML = '<img src="images/morning.png" alt="Foto Dia"></img>'
+    }
 }
