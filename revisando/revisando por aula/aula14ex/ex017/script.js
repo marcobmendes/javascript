9318249e3ca55c9e352b9811b42f0131e50019c7
Marco Antonio Brandini Mendes <137082335+marcobmendes@users.noreply.github.com>
2024-03-29 07:14:04 -0300

    Revisando os Exercícios (Parte 6).
    Finalizando o ex017.

--- revisando/revisando por aula/aula14ex/ex017/script.js
+++ revisando/revisando por aula/aula14ex/ex017/script.js
@@ -2,13 +2,17 @@ function gerar() {
     let txtn = document.getElementById('txtn')
     let res = document.getElementById('tab')
 
-    if (txtn.value.lenght == 0) {
+    if (txtn.value.length == 0) {
         window.alert('[ERRO] Digite um número!')
     } else {
         var n1 = Number(txtn.value)
         res.innerHTML = ''
         for (let c = 1; c <= 10; c++) {
-            res.innerText = `${n1} x ${c} = ${(n1 * c)}`            
+            // res.innerText = `${n1} x ${c} = ${(n1 * c)}`
+            let item = document.createElement('option')  
+            item.text = `${n1} x ${c} = ${(n1 * c)}`
+            item.value = `tab${c}`
+            res.appendChild(item)  
         }
     }
 }
