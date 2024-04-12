192d1215f06dea96a52fc6223d26d9f9ca5270d1
Marco Antonio Brandini Mendes <137082335+marcobmendes@users.noreply.github.com>
2024-04-11 21:10:51 -0300

    Finalizando o ex022.
    Referente a Aula "Exercícios JavaScript (Parte 8)".

--- exercicios/ex022/script.js
+++ exercicios/ex022/script.js
@@ -21,11 +21,42 @@ function inLista(n, l) {
 
 function adicionar() {
     if (isNumero(txtn.value) && !inLista(txtn.value, valores)) {
-        
+        valores.push(Number(txtn.value))
+        let item = document.createElement('option')
+        item.text += `Valor ${txtn.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('ERRO! Valor inválido ou já encontrado na lista!')
     }
-    
+    txtn.value = ''
+    txtn.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de f')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados. </p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media.toFixed(2)}.</p>`
+    }
 }
 
 /*
